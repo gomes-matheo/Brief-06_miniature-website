6994b930352c645ce42b83d49f7080e84b47c354
PEREIRA Arthur <arthur.pereira@externe.maif.fr>
2026-03-11 11:29:44 +0100

    Création User Post et index

---
+++ app/src/main/models/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    private int id = 0;
+    private int nbrPosts=0;
+    private User owner;
+    private Post parent;
+    private String content;
+    private Date createdAt;
+    private boolean isDraft = false;
+    
+}
